4133acea347c61199459f399263ffa67b3f5a58b
whinc <xiaohui_hubei@163.com>
2017-08-08 00:06:36 +0800

    Add - 添加首页代码

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to github</h2>
+      <div style={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
+        <div style={{display: 'flex', height: '44px', backgroundColor: 'black', alignItems: 'center', paddingLeft: '10px'}}>
+          <span style={{color: 'white', fontSize: '18px'}}>title</span>
         </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div style={{flexGrow: 1}}>A</div>
+        <div style={{display: 'flex', color: 'red', height: '44px', alignItems: 'center'}}>
+          <button style={{flexGrow: 1, height: '100%'}}>A</button>
+          <button style={{flexGrow: 1, height: '100%'}}>B</button>
+          <button style={{flexGrow: 1, height: '100%'}}>C</button>
+          <button style={{flexGrow: 1, height: '100%'}}>D</button>
+        </div>
       </div>
     )
   }
